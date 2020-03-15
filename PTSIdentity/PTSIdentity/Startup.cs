ecf9d66ec706491eeb77eb7c4aed4d00e5bddc47
David Forshaw <davidbforshaw@outlook.com>
2020-03-15 16:21:50 +0000

    Initial Project creation and User Sign up

---
+++ PTSIdentity/PTSIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PTSIdentity.Startup))]
+namespace PTSIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
