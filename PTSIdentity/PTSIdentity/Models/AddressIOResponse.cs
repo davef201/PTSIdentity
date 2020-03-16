8baa1a4d4fc4299f95de43e157b5a6f53725cbe3
David Forshaw <davidbforshaw@outlook.com>
2020-03-16 02:33:33 +0000

    Added Postcode valdation with GetAddressIO

---
+++ PTSIdentity/PTSIdentity/Models/AddressIOResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTSIdentity.Models
+{
+    public class AddressIOResponse
+    {
+        public string[] addresses { get; set; }
+    }
+}
